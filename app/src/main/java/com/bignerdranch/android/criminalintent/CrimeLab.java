18ea94fb85ed66cf677d3e3872d085b87a7bbfa9
Steven Spencer <sspencer@playnetwork.com>
2016-02-16 16:56:20 -0800

    add toolbar

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -23,12 +23,18 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+        /*
         for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + (i+1));
             crime.setSolved(i % 2 == 0);
             mCrimes.add(crime);
         }
+        */
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
     public List<Crime> getCrimes() {
